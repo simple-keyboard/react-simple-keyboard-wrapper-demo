c1a7b066366a4ff664c42437ac5349790ddad57a
simple-keyboard <46766174+simple-keyboard@users.noreply.github.com>
2019-03-07 23:04:35 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+/**
+ * 3/7/2019 Outdated. Please wait for update on 3/8/2019
+ */
 import React, { Component } from "react";
 import { render } from "react-dom";
 import KeyboardWrapper from "./KeyboardWrapper.js";
