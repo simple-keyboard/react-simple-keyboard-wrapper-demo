08872b98ad9840983f13238314fb9bc866cdfbc8
simple-keyboard <46766174+simple-keyboard@users.noreply.github.com>
2019-09-07 00:42:03 -0400

    Update KeyboardWrapper.js

--- src/KeyboardWrapper.js
+++ src/KeyboardWrapper.js
@@ -11,7 +11,7 @@ class KeyboardWrapper extends Component {
   }
 
   render() {
-    return <Keyboard ref={r => (this.keyboardRef = r)} {...this.props} />;
+    return <Keyboard keyboardRef={r => (this.keyboard = r)} {...this.props} />;
   }
 }
 
